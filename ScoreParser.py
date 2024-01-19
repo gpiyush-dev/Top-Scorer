80769fa0a6a600e3bd7a5e45ad363de2703b28b0
Piyush Gupta <piyush171098@gmail.com>
2024-01-19 11:10:53 +0530

    Update ScoreParser.py

--- ScoreParser.py
+++ ScoreParser.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("C:\Users\Piyush\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\site-packages")
+sys.path.append("C:\\Users\\Piyush\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python312\\site-packages")
 
 import pandas as pd
 
